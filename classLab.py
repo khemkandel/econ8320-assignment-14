15b4941d17a3a353c14f6bb2475f31ad0f5d05aa
Khem <khemkandel@gmail.com>
2025-05-03 11:39:14 -0500

    Testing Time

--- classLab.py
+++ classLab.py
@@ -1,5 +1,4 @@
 #si-exercise
-import numpy as np
 import multiprocess as mp # This module is part of the
                           # python standard library
 import timeit             # library for timing execution of code
@@ -10,6 +9,7 @@ def f(alpha,x1,x2,x3,epsilon):
 
 
 def average_y(nSample,f):
+  import numpy as np
   alpha = np.random.normal(15,2,nSample) 
   #alpha[:10]
 
@@ -41,15 +41,19 @@ def pCalc(processes, n_bins, n_reps, f):
 
 
 sResults = sCalc(1000,100,f)
-pResults = pCalc(1000,100,f)
+pResults = pCalc(2,1000,100,f)
 
 benchmarks = [] # list to store our execution times
 
-benchmarks.append(timeit.Timer('serial_average(10000, 100, f, 0, 1)','from __main__ import serial_average, serial_integral, f').timeit(number=1))
+benchmarks.append(timeit.Timer('sCalc(10000, 100, f)','from __main__ import sCalc, average_y, f').timeit(number=1))
     # Note that we need to include a second line
     # that imports our functions from __main__.
     # This tells the timer what needs to be IN SCOPE
 
-benchmarks.append(timeit.Timer('parallel_average(2, 10000, 100, f, 0, 1)','from __main__ import parallel_average, serial_integral, f').timeit(number=1))
+benchmarks.append(timeit.Timer('pCalc(2, 10000, 100, f)','from __main__ import pCalc, average_y, f').timeit(number=1))
     # Need to include number of processes
     # when timing the parallel implementation
+
+
+stime = benchmarks[0]
+ptime = benchmarks[1]
